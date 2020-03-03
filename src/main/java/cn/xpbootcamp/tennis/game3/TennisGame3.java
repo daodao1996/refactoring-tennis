4cfb2d7f86714a6ff25394ef8b9f8c1f797ce887
daodao1996 <yunpeng.ding@thoughtworks.com>
2020-03-03 22:23:19 +0800

    extract method

--- src/main/java/cn/xpbootcamp/tennis/game3/TennisGame3.java
+++ src/main/java/cn/xpbootcamp/tennis/game3/TennisGame3.java
@@ -4,34 +4,45 @@ import cn.xpbootcamp.tennis.TennisGame;
 
 public class TennisGame3 implements TennisGame {
 
-    private int player2Point;
-    private int player1Point;
-    private String player1Name;
-    private String player2Name;
+  private int player2Point;
+  private int player1Point;
+  private String player1Name;
+  private String player2Name;
 
-    public TennisGame3(String p1N, String p2N) {
-        this.player1Name = p1N;
-        this.player2Name = p2N;
-    }
+  public TennisGame3(String p1N, String p2N) {
+    this.player1Name = p1N;
+    this.player2Name = p2N;
+  }
 
-    @Override
-    public void setPlayerPoint(int player1Point, int player2Point) {
-        this.player1Point = player1Point;
-        this.player2Point = player2Point;
-    }
+  @Override
+  public void setPlayerPoint(int player1Point, int player2Point) {
+    this.player1Point = player1Point;
+    this.player2Point = player2Point;
+  }
 
-    public String getScore() {
-        String s;
-        if (player1Point < 4 && player2Point < 4 && !(player1Point + player2Point == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-            s = p[player1Point];
-            return (player1Point == player2Point) ? s + "-All" : s + "-" + p[player2Point];
-        } else {
-            if (player1Point == player2Point)
-                return "Deuce";
-            s = player1Point > player2Point ? player1Name : player2Name;
-            return ((player1Point - player2Point)*(player1Point - player2Point) == 1) ? "Advantage " + s : "Win for " + s;
-        }
+  public String getScore() {
+    String score;
+    String[] description = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+    if (isHighScore()) {
+      if (isDraw())
+        return "Deuce";
+      return (Math.abs(player1Point - player2Point) == 1) ? "Advantage " + getWinner() : "Win for " + getWinner();
+    } else {
+      score = description[player1Point];
+      return (isDraw()) ? score + "-All" : score + "-" + description[player2Point];
     }
+  }
+
+  private boolean isHighScore() {
+    return player1Point >= 4 || player2Point >= 4 || player1Point + player2Point == 6;
+  }
+
+  private String getWinner() {
+    return player1Point > player2Point ? player1Name : player2Name;
+  }
+
+  private boolean isDraw() {
+    return player1Point == player2Point;
+  }
 
 }
