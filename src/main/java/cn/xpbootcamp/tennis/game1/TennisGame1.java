7190c0aca89c6ee83bfc0db69682770f4c4a7d60
daodao1996 <yunpeng.ding@thoughtworks.com>
2020-03-03 21:39:20 +0800

    extract getPointDescription method

--- src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
+++ src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
@@ -4,73 +4,53 @@ import cn.xpbootcamp.tennis.TennisGame;
 
 public class TennisGame1 implements TennisGame {
 
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+  private int m_score1 = 0;
+  private int m_score2 = 0;
+  private String player1Name;
+  private String player2Name;
 
-    public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
-    }
+  public TennisGame1(String player1Name, String player2Name) {
+    this.player1Name = player1Name;
+    this.player2Name = player2Name;
+  }
 
-    @Override
-    public void setPlayerPoint(int p1Point, int p2Point) {
-        this.m_score1 = p1Point;
-        this.m_score2 = p2Point;
-    }
+  @Override
+  public void setPlayerPoint(int p1Point, int p2Point) {
+    this.m_score1 = p1Point;
+    this.m_score2 = p2Point;
+  }
 
-    public String getScore() {
-        StringBuilder score = new StringBuilder();
-        int tempScore = 0;
-        if (m_score1 == m_score2) {
-            getScoreDescription(m_score1, score);
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
-            int minusResult = m_score1 - m_score2;
-            if (minusResult == 1) score = new StringBuilder("Advantage player1");
-            else if (minusResult == -1) score = new StringBuilder("Advantage player2");
-            else if (minusResult >= 2) score = new StringBuilder("Win for player1");
-            else score = new StringBuilder("Win for player2");
-        } else {
-            for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = m_score1;
-                else {
-                    score.append("-");
-                    tempScore = m_score2;
-                }
-                switch (tempScore) {
-                    case 0:
-                        score.append("Love");
-                        break;
-                    case 1:
-                        score.append("Fifteen");
-                        break;
-                    case 2:
-                        score.append("Thirty");
-                        break;
-                    case 3:
-                        score.append("Forty");
-                        break;
-                }
-            }
-        }
-        return score.toString();
-    }
+  public String getScore() {
+    StringBuilder score = new StringBuilder();
 
-    private void getScoreDescription(int score, StringBuilder description) {
-        switch (score) {
-            case 0:
-                description.append("Love-All");
-                break;
-            case 1:
-                description.append("Fifteen-All");
-                break;
-            case 2:
-                description.append("Thirty-All");
-                break;
-            default:
-                description.append("Deuce");
-                break;
-        }
+    if (m_score1 == m_score2) {
+      if (m_score1 > 2)
+        return "Deuce";
+      else {
+        return getPointDescription(m_score1) + "-All";
+      }
+    } else if (m_score1 >= 4 || m_score2 >= 4) {
+      int minusResult = m_score1 - m_score2;
+      if (minusResult == 1) score = new StringBuilder("Advantage player1");
+      else if (minusResult == -1) score = new StringBuilder("Advantage player2");
+      else if (minusResult >= 2) score = new StringBuilder("Win for player1");
+      else score = new StringBuilder("Win for player2");
+    } else {
+      return getPointDescription(m_score1) + "-" + getPointDescription(m_score2);
     }
+    return score.toString();
+  }
+
+  private String getPointDescription(int point) {
+    switch (point) {
+      case 0:
+        return "Love";
+      case 1:
+        return "Fifteen";
+      case 2:
+        return "Thirty";
+      default:
+        return "Forty";
+    }
+  }
 }
